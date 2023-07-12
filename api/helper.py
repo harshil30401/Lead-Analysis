353fde50ac55a9100bb3b486971dce1dd530907b
harshil30401 <harshilpatel30401@gmail.com>
2023-07-12 16:57:48 +0530

    First commit

---
+++ api/helper.py
@@ -0,0 +1,93 @@
+import openai
+import json
+import os
+import validators
+import requests
+from fastapi import HTTPException
+from io import BytesIO
+from typing import Dict, Union
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+openai.api_key = OPENAI_API_KEY
+
+
+functions = [
+    {
+        "name": "call_analysis",
+        "description": "Shows a detailed analysis of the call.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "call_summary": {
+                    "type": "string",
+                    "description": "Detailed summary of the call.",
+                },
+                "next_action_item": {
+                    "type": "string",
+                    "description": "Detailed analysis of the next action item? (In Detail)",
+                },
+                "customer_sentiment": {
+                    "type": "string",
+                    "description": "Detailed analysis of the customer's sentiment? (In Detail)",
+                },
+                "salesperson_performance": {
+                    "type": "string",
+                    "description": "Detailed analysis of the performance of the salesperson? ",
+                },
+            },
+        },
+    }
+]
+
+
+def convert_url(url: str) -> Union[BytesIO, HTTPException]:
+    if not validators.url(url):
+        raise HTTPException(
+            status_code=400,
+            detail="The server cannot process your request because the provided URL syntax is invalid or malformed!",
+        )
+
+    if not url.endswith(".mp3"):
+        raise HTTPException(
+            status_code=400, detail="The provided URL is not an MP3 file!"
+        )
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        mp3_content = response.content
+        buffer = BytesIO(mp3_content)
+        buffer.name = "temp.mp3"
+        return buffer
+    elif response.status_code == 404:
+        raise HTTPException(
+            status_code=404, detail="The input resource could not be found!"
+        )
+
+
+def get_analysis(audio_file) -> Dict[str, str]:
+    call_log = openai.Audio.translate("whisper-1", audio_file)
+
+    transcript = call_log["text"]
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": """You are a helpful real-estate sales assistant. Based on the transcript log between a human salesperson and a customer, answer the following questions:
+        1. Summary of the call
+        2. What is the next action item?
+        3. What is the customer's sentiment?
+        4. How was the performance of the salesperson
+        """,
+            },
+            {"role": "user", "content": f"{transcript}"},
+        ],
+        functions=functions,
+        function_call={"name": "call_analysis"},
+    )
+
+    arguments = completion["choices"][0]["message"]["function_call"]["arguments"]
+    json_object = json.loads(arguments)
+    return json_object
---
+++ api/main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+from typing import Dict
+from models import AudioRequest, AudioResponse
+from helper import get_analysis, convert_url
+
+
+app = FastAPI(
+    title="EchoSensai",
+    description="An advanced AI-powered call analysis API designed to provide comprehensive insights and intelligent recommendations for your conversations.",
+    version="1.0.0",
+    openapi_tags=[
+        {"name": "Call Analysis", "description": "Endpoints for call analysis"},
+    ],
+)
+
+
+@app.get("/", tags=["Index"])
+def index():
+    return {"message": "EchoSensai"}
+
+
+@app.post("/get_call_analysis", tags=["Call Analysis"], response_model=AudioResponse)
+def process(audio_url: AudioRequest) -> Dict[str, str]:
+    analysis = get_analysis(convert_url(audio_url.mp3_url))
+    return analysis
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
