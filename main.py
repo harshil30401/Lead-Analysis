2de43460b7aa7c551879b607f246017d7aa9bed8
sqy-harshil <harshil.patel@squareyards.co.in>
2023-08-22 13:42:54 +0530

    Added second endpoint

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+from typing import Dict
+
 import uvicorn
 from fastapi import FastAPI, HTTPException, Request, Security, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.security import APIKeyHeader
-from typing import Dict
-from models import AudioRequest, AudioResponse
-from helper import get_analysis, convert_url
+
+from models import AudioRequest, SimpleAudioResponse, DetailedAudioResponse
+from helper import get_analysis_4, get_analysis_8, convert_url
 
 app = FastAPI(
     title="EchoSensai",
@@ -24,6 +26,7 @@ key = os.getenv("CALL_ANALYSIS_API_KEY")
 # Custom API key header
 api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
+
 # Dependency function to validate API key
 async def get_api_key(api_key_header: str = Security(api_key_header)):
     if api_key_header:
@@ -39,11 +42,34 @@ async def get_api_key(api_key_header: str = Security(api_key_header)):
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/get_call_analysis", tags=["Call Analysis"], response_model=AudioResponse)
-def process(audio_url: AudioRequest, api_key: str = Depends(get_api_key)) -> Dict[str, str]:
-    print(f"Using audio file at: {audio_url.mp3_url}")
-    analysis = get_analysis(convert_url(audio_url.mp3_url))
-    return analysis
+
+@app.post(
+    "/get_simple_call_analysis",
+    tags=["Call Analysis"],
+    response_model=SimpleAudioResponse,
+    description="Get a call analysis of an audio input on 4 parameters",
+    name="Echo-Quad-Sensai",
+)
+def process(
+    audio_url: AudioRequest, api_key: str = Depends(get_api_key)
+) -> Dict[str, str]:
+    analysis = get_analysis_4(convert_url(audio_url.mp3_url))
+    return analysis.json_object
+
+
+@app.post(
+    "/get_detailed_call_analysis",
+    tags=["Call Analysis"],
+    response_model=DetailedAudioResponse,
+    description="Get a call analysis of an audio input on 8 parameters (This is currently in prototyping phase)",
+    name="Echo-Octa-Sensai (Beta)",
+)
+def process(
+    audio_url: AudioRequest, api_key: str = Depends(get_api_key)
+) -> Dict[str, str]:
+    analysis = get_analysis_8(convert_url(audio_url.mp3_url))
+    return analysis.json_object
+
 
 if __name__ == "__main__":
     with app:
